adcdad41f3ce5ab43f02e8ec05d00f56fcf20c10
bleedred23 <yellow.stars@yandex.ru>
2023-07-27 03:07:44 +0500

    Constructor

--- commands/ConstructController.php
+++ commands/ConstructController.php
@@ -3,51 +3,23 @@
 namespace app\commands;
 
 use app\models\Construct;
-use app\models\Ingredient;
-use app\models\IngredientType;
 use yii\console\Controller;
 use yii\console\ExitCode;
 
 class ConstructController extends Controller
 {
-    /**
-     * This command echoes what you have entered as the message.
-     * @return int Exit code
-     */
+
     public function actionIndex(string $ingredients): int
     {
         $model = new Construct(['ingredientsTypeList' => $ingredients]);
-        $model->calc();
-        return ExitCode::OK;
-        $ingredients = str_split($ingredients);
-        $uniqIngredients = array_unique($ingredients);
 
-        $ingredientTypes = IngredientType::find()
-            ->select(['id', 'code'])
-            ->where(['code' => $uniqIngredients])
-            ->indexBy('id')
-            ->asArray()
-            ->all();
-        var_dump($ingredientTypes);
-
-        $typeCodes = [];
-        foreach ($ingredientTypes as $type) {
-            $typeCodes[$type['id']] = $type['code'];
+        try {
+            $output = $model->calc();
+            $this->stdout($output);
+        } catch (\Exception $e) {
+            $this->stderr($e->getMessage());
+            return $e->getCode();
         }
-
-        foreach ($uniqIngredients as $ingredient) {
-            if (!in_array($ingredient, $typeCodes)) {
-                $this->stderr("Ingredient $ingredient is not present at list");
-                return ExitCode::DATAERR;
-            }
-        }
-
-        $allIngredients = Ingredient::find()
-            ->where(['type_id' => array_keys($typeCodes)])
-            ->asArray()
-            ->all();
-
-        $allIngredients = Ingredient::findAll(['type_id']);
 
         return ExitCode::OK;
     }
--- models/Construct.php
+++ models/Construct.php
@@ -2,66 +2,93 @@
 
 namespace app\models;
 
+use yii\base\InvalidArgumentException;
 use yii\base\Model;
 use yii\console\ExitCode;
+use yii\helpers\Json;
 
 class Construct extends Model
 {
     private array $ingredientsByCode = [];
     public string $ingredientsTypeList = '';
 
+    /**
+     * Вычисляет все возможные комбинации ингредиентов
+     *
+     * @return string
+     */
     public function calc()
     {
+        if (!$this->ingredientsTypeList) {
+            throw new InvalidArgumentException("No ingredients passed", ExitCode::DATAERR);
+        }
         $this->prepareData();
-        $usedIngredients = [];
-        $resultData = [
-            [
-                'products' => [],
-                'price' => 0
-            ]
-        ];
+        $resultData = [];
 
-        $used = [];
-//        var_dump(str_split($this->ingredientsTypeList)); die();
         foreach (str_split($this->ingredientsTypeList) as $i => $code) {
-            $newResultData = [];
-            $k = 0;
+            $resultDataTpl = $resultData;
+            $resultData = [];
             foreach ($this->ingredientsByCode[$code] as $ingredient) {
-                if (in_array($ingredient['id'], $used)) {
+                if ($i === 0) {
+                    $resultData[] = [
+                        'products' => [[
+                            'type' => $ingredient['type'],
+                            'value' => $ingredient['title'],
+                        ]],
+                        'price' => $ingredient['price'],
+                        'used' => [$ingredient['id']],
+                    ];
+
                     continue;
                 }
 
-                $resultDataBefore = $resultData;
-                $used[] = $ingredient['id'];
-                foreach ($resultDataBefore as &$data) {
-                    $data['products'][$i] = [
+                $newData = $resultDataTpl;
+                foreach ($newData as &$data) {
+                    // Не позволяем использовать несколько ингредиентов
+                    // в одной сборке
+                    if (in_array($ingredient['id'], $data['used'])) {
+                        continue;
+                    }
+                    $data['products'][] = [
                         'type' => $ingredient['type'],
                         'value' => $ingredient['title'],
                     ];
                     $data['price'] += $ingredient['price'];
+                    $data['used'][] = $ingredient['id'];
                 }
                 unset($data);
-                if ($k === 0) {
-                    $newResultData = $resultDataBefore;
-                    $k++;
-                } {
 
-                $newResultData[] = $resultDataBefore;
-                }
-            }
-
-            var_dump([
-                $resultData,
-                $newResultData,
-            ]);
-            if ($newResultData) {
-                $resultData = $newResultData;
+                $resultData = array_merge($resultData, $newData);
             }
         }
 
-//        var_dump($resultData);
+        $used = [];
+        foreach ($resultData as $k => $data) {
+            if (count($data['products']) != strlen($this->ingredientsTypeList)) {
+                unset($resultData[$k]);
+            }
+
+            // Исключаем из списка дублирующие подборки,
+            // в которых перетасованы ингредиенты
+            sort($data['used']);
+            $items = implode(',', $data['used']);
+            if (isset($used[$items])) {
+                unset($resultData[$k]);
+            }
+            unset($resultData[$k]['used']);
+
+            $used[$items] = true;
+        }
+
+        sort($resultData);
+        return Json::encode($resultData);
     }
 
+    /**
+     * Заполняет $this->ingredientsByCode необходимыми ингредиентами
+     *
+     * @return void
+     */
     private function prepareData(): void
     {
         $ingredients = str_split($this->ingredientsTypeList);
@@ -71,6 +98,7 @@ class Construct extends Model
             ->select(['id', 'code'])
             ->where(['code' => $uniqIngredients])
             ->indexBy('id')
+            ->cache(300)
             ->asArray()
             ->all();
 
@@ -82,7 +110,7 @@ class Construct extends Model
 
         foreach ($uniqIngredients as $ingredient) {
             if (!in_array($ingredient, $typeCodes)) {
-                $this->addError('ingredients', "Ingredient $ingredient is not present at list");
+                throw new InvalidArgumentException("Ingredient type $ingredient is not present at list", ExitCode::DATAERR);
             }
         }
 
@@ -98,11 +126,22 @@ class Construct extends Model
                 'ingredient_type it',
                 'i.type_id = it.id'
             )
+            ->cache(300)
             ->asArray()
             ->all();
 
         foreach ($allIngredients as $ingredient) {
             $this->ingredientsByCode[$ingredient['code']][] = $ingredient;
+        }
+
+        // Т.к. блюдо не может содержать в себе несколько одинаковых ингредиентов - проверяем,
+        // что у нас хватит уникальных ингредиентов для каждого типа
+        foreach ($typeCodes as $code) {
+            $count = substr_count($this->ingredientsTypeList, $code);
+            $maxCount = count($this->ingredientsByCode[$code]);
+            if ($count > $maxCount) {
+                throw new InvalidArgumentException("Dish can't contain more than $maxCount ingredients with type $code", ExitCode::DATAERR);
+            }
         }
     }
 }
